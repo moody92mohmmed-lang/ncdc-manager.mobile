ec6f2356333481b3aedae1e30784e374b51eeec8
moody92mohmmed-lang <moody92mohmmed@gmail.com>
2026-05-31 11:26:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ncdc-manager-mobile-v4';
+const CACHE_NAME = 'ncdc-manager-mobile-v5';
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())); });
 self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).catch(() => caches.match(event.request))); });
